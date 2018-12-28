57bbe68bb13d17e669fdac09fe9ca18e176bc06a
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2018-12-29 01:31:04 +0900

    Initial Commit

---
+++ xcode_gen_exampleTests/xcode_gen_exampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  xcode_gen_exampleTests.swift
+//  xcode_gen_exampleTests
+//
+//  Created by 平塚 俊輔 on 2018/12/29.
+//  Copyright © 2018 平塚 俊輔. All rights reserved.
+//
+
+import XCTest
+@testable import xcode_gen_example
+
+class xcode_gen_exampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
