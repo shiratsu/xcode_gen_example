d62ec7fccb2a187745c70ac8a7c7e15d73ba48a1
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2018-12-29 01:45:25 +0900

    initial commit

---
+++ hiratsuka-test/hiratsuka_test.h
@@ -0,0 +1,19 @@
+//
+//  hiratsuka_test.h
+//  hiratsuka-test
+//
+//  Created by 平塚 俊輔 on 2018/12/29.
+//  Copyright © 2018 平塚 俊輔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hiratsuka_test.
+FOUNDATION_EXPORT double hiratsuka_testVersionNumber;
+
+//! Project version string for hiratsuka_test.
+FOUNDATION_EXPORT const unsigned char hiratsuka_testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hiratsuka_test/PublicHeader.h>
+
+
